80915da66635bdddcd206614b9e28aee0cc96adf
brainysoon <brainysoon@gmail.com>
2018-03-04 22:32:03 +0800

    add-material-ui-app-bar

--- eve/src/launchHome/index.jsx
+++ eve/src/launchHome/index.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+import withRoot from '../share/withRoot';
+import AppBar from 'material-ui/AppBar';
+import ToolBar from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui/Menu';
+import Typography from 'material-ui/Typography';
+import {withStyles} from 'material-ui/styles';
+
+const styles = {};
 
 class Home extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {open: false}
+    }
+
     render() {
 
-        return <div>Hello Eve!</div>;
+        const {open} = this.state;
+
+        return <AppBar>
+            <ToolBar>
+                <IconButton>
+                    <MenuIcon open={open}/>
+                </IconButton>
+                <Typography>
+                    首页
+                </Typography>
+            </ToolBar>
+        </AppBar>;
     }
 }
 
-export default Home;
+Home.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default _.flowRight(
+    withRoot,
+    withStyles(styles)
+)(Home);
