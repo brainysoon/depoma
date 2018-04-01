e475ff96a7b309a63c4414b2584896af3d93c279
brainysoon <brainysoon@gmail.com>
2018-04-01 23:17:30 +0800

    Front-1 - Add drawer open and close logic.

--- eve/src/launchHome/page/homePage.jsx
+++ eve/src/launchHome/page/homePage.jsx
@@ -6,34 +6,47 @@ import AppBar from 'material-ui/AppBar';
 import ToolBar from 'material-ui/Toolbar';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
+import Drawer from 'material-ui/Drawer';
 import Typography from 'material-ui/Typography';
+import Divider from 'material-ui/Divider';
+import List from 'material-ui/List';
 import {withStyles} from 'material-ui/styles';
 import {COLOR_INHERIT} from 'src/share/util/constant/constants';
 
 const styles = {};
 
 class Home extends React.Component {
-
     constructor(props) {
         super(props);
+        this.state = {open: false}
+    }
 
-        this.state = {open: true}
+    _handleMenuClick = () => {
+        this.setState({open: !this.state.open})
     }
 
     render() {
-
         const {open} = this.state;
 
-        return <AppBar>
-            <ToolBar>
-                <IconButton color={COLOR_INHERIT}>
-                    <MenuIcon open={open}/>
-                </IconButton>
-                <Typography color={COLOR_INHERIT}>
-                    首页
-                </Typography>
-            </ToolBar>
-        </AppBar>;
+        return <div>
+            <AppBar>
+                <ToolBar>
+                    <IconButton color={COLOR_INHERIT}>
+                        <MenuIcon open={open} onClick={this._handleMenuClick}/>
+                    </IconButton>
+                    <Typography color={COLOR_INHERIT}>
+                        首页
+                    </Typography>
+                </ToolBar>
+            </AppBar>
+            <Drawer anchor='left' open={open}>
+                <div>
+                    <List>Brainy Soon</List>
+                    <Divider/>
+                    <List>Guo Ting</List>
+                </div>
+            </Drawer>
+        </div>;
     }
 }
 
