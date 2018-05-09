382aa7641cc2042c0c82d327fe11dd1eedc4c16d
brainysoon <brainysoon@gmail.com>
2018-05-09 16:54:39 +0800

    [ Front-5 ] - Add app menuStatus and bottomNavCheckedIndex reducer.

--- eve/src/bootstrap/reducer/appReducer.js
+++ eve/src/bootstrap/reducer/appReducer.js
@@ -1,11 +1,30 @@
 import {combineReducers} from 'redux';
+import {TOGGLE_MENU_STATUS, BOTTOM_NAV_CHECKED_INDEX_CHANGE} from 'src/share/actionType/sharedActionTypes';
+import {DEFAULT_BOTTOM_NAV_CHECKED_INDEX, DEFAULT_MENU_STATUS} from 'src/share/constant/sharedConstants';
 
-const app = (state={}, action) => {
+const menuStatus = (state = DEFAULT_MENU_STATUS, action) => {
 
-    return state;
+    switch (action.type) {
+        case TOGGLE_MENU_STATUS:
+            return !state;
+        default:
+            return state;
+    }
 };
+
+const bottomNavCheckedIndex = (state = DEFAULT_BOTTOM_NAV_CHECKED_INDEX, action) => {
+
+    switch (action.type) {
+        case BOTTOM_NAV_CHECKED_INDEX_CHANGE:
+            return action.bottomNavCheckedIndex;
+        default:
+            return state;
+    }
+};
+
 const appReducer = combineReducers({
-    app
+    bottomNavCheckedIndex,
+    menuStatus
 });
 
 export default appReducer;
---
+++ eve/src/share/constant/sharedConstants.js
@@ -0,0 +1,2 @@
+export const DEFAULT_BOTTOM_NAV_CHECKED_INDEX = 0;
+export const DEFAULT_MENU_STATUS = false;
