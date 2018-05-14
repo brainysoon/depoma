f1a329dae80fa4c1f35c510c8b3256efa95be672
brainysoon <brainysoon@gmail.com>
2018-05-14 17:44:53 +0800

    [ Front-5 ] - Add all four menu mapping.

--- eve/src/share/constant/uriConstants.js
+++ eve/src/share/constant/uriConstants.js
@@ -1,2 +1,4 @@
 export const HOME = '/';
 export const ROBOT = '/robot';
+export const CONTENT = '/content';
+export const LOG = '/log';
--- eve/src/share/util/frameContainerUtils.js
+++ eve/src/share/util/frameContainerUtils.js
@@ -1,13 +1,17 @@
 // @flow
-import {HOME, ROBOT} from 'src/share/constant/uriConstants';
+import {HOME, ROBOT, CONTENT, LOG} from 'src/share/constant/uriConstants';
 
 export const mapMenuIndexToURL = (index: number): string => {
 
     switch (index) {
         case 0:
-            return ROBOT;
-        case 1:
             return HOME;
+        case 1:
+            return ROBOT;
+        case 2:
+            return CONTENT;
+        case 3:
+            return LOG;
         default:
             return HOME;
     }
