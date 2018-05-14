373d28607cc577ebf6eba53f326e48a8e65a0b1b
brainysoon <brainysoon@gmail.com>
2018-05-14 18:19:10 +0800

    [ Front-5 ] - Adding four uri to app router.

--- eve/src/bootstrap/route/appRoute.jsx
+++ eve/src/bootstrap/route/appRoute.jsx
@@ -1,11 +1,18 @@
 import {Route, Switch} from 'react-router';
 import React from 'react';
+import URIConstants from 'src/share/constant/uriConstants';
 
 import Profile from 'src/profile';
+import Robot from 'src/robot';
+import Log from 'src/log';
+import Content from 'src/content';
 
 const appRoute = (
     <Switch>
-        <Route path='/' component={Profile}/>
+        <Route exect path={URIConstants.HOME} component={Profile}/>
+        <Route path={URIConstants.CONTENT} component={Content}/>
+        <Route path={URIConstants.ROBOT} component={Robot}/>
+        <Route path={URIConstants.LOG} component={Log}/>
     </Switch>);
 
 export default appRoute;
