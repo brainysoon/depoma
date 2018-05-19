5c92936a75383e2b94b256375172b4a75f58e4bb
brainysoon <brainysoon@gmail.com>
2018-05-19 14:05:15 +0800

    [ Front-5 ] - Add wechatLoginState to redux.

--- eve/src/bootstrap/reducer/appReducer.js
+++ eve/src/bootstrap/reducer/appReducer.js
@@ -1,6 +1,14 @@
 import {combineReducers} from 'redux';
-import {TOGGLE_MENU_STATUS, BOTTOM_NAV_CHECKED_INDEX_CHANGE} from 'src/share/actionType/sharedActionTypes';
-import {DEFAULT_BOTTOM_NAV_CHECKED_INDEX, DEFAULT_MENU_STATUS} from 'src/share/constant/sharedConstants';
+import {
+    TOGGLE_MENU_STATUS,
+    BOTTOM_NAV_CHECKED_INDEX_CHANGE,
+    WECHAT_LOGIN_STATE_CHANGE
+} from 'src/share/actionType/sharedActionTypes';
+import {
+    DEFAULT_BOTTOM_NAV_CHECKED_INDEX,
+    DEFAULT_MENU_STATUS,
+    DEFAULT_WECHAT_LOGIN_STATE
+} from 'src/share/constant/sharedConstants';
 
 const menuStatus = (state = DEFAULT_MENU_STATUS, action) => {
 
@@ -22,9 +30,20 @@ const bottomNavCheckedIndex = (state = DEFAULT_BOTTOM_NAV_CHECKED_INDEX, action)
     }
 };
 
+const wechatLoginState = (state = DEFAULT_WECHAT_LOGIN_STATE, action) => {
+
+    switch (action.type) {
+        case WECHAT_LOGIN_STATE_CHANGE:
+            return action.wechatLoginState;
+        default:
+            return state;
+    }
+};
+
 const appReducer = combineReducers({
     bottomNavCheckedIndex,
-    menuStatus
+    menuStatus,
+    wechatLoginState
 });
 
 export default appReducer;
--- eve/src/share/constant/sharedConstants.js
+++ eve/src/share/constant/sharedConstants.js
@@ -1,2 +1,3 @@
 export const DEFAULT_BOTTOM_NAV_CHECKED_INDEX = 0;
 export const DEFAULT_MENU_STATUS = false;
+export const DEFAULT_WECHAT_LOGIN_STATE = false;
