ca3f1699640c49bb05b9d66f3e051cea29eed2f1
brainysoon <brainysoon@gmail.com>
2018-06-01 08:48:30 +0800

    [ Back-6 ] - Add multi user support.

--- mo/flaskr/api.py
+++ mo/flaskr/api.py
@@ -16,7 +16,7 @@ def login():
     if request.method == 'POST':
         wechat_id = str(uuid.uuid1())
 
-        login_thread = wechat.wechat_login(1, 'test', wechat_id)
+        login_thread = wechat.wechat_login('test', wechat_id)
         login_thread.start()
 
         pic_link = env.server_resources_prefix() + '/qr/' + wechat_id + '.png'
--- mo/flaskr/wechat.py
+++ mo/flaskr/wechat.py
@@ -4,13 +4,13 @@ from . import env
 
 
 class wechat_login(threading.Thread):
-    def __init__(self, thread_id, name, wechat_id):
+    def __init__(self, name, wechat_id):
         threading.Thread.__init__(self)
-        self.thread_id = thread_id
-        self.name = name
-        self.pic_dir = env.QR_SAVE_DIR_PRE_FIX + wechat_id+'.png'
+        self.name = name + wechat_id
+        self.pic_dir = env.QR_SAVE_DIR_PRE_FIX + wechat_id + '.png'
 
     def run(self):
         print("开始线程：" + self.name)
-        itchat.login(picDir=self.pic_dir)
+        wechat_instance = itchat.new_instance()
+        wechat_instance.login(picDir=self.pic_dir)
         print("退出线程：" + self.name)
