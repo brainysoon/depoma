90b88a3e31672b55c855d5ac46dadc75709ab97a
brainysoon <brainysoon@gmail.com>
2018-06-02 23:55:42 +0800

    [ Back-6 ] - Add chat records display.

--- eve/src/log/page/logPage.jsx
+++ eve/src/log/page/logPage.jsx
@@ -10,6 +10,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import {withStyles} from "@material-ui/core/styles/index";
 import {getWechatId} from 'src/log/selector/logPageSelector';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 type Props = {
     menuStatus: boolean,
@@ -18,17 +19,24 @@ type Props = {
     handleBottomNavClick: (number) => void,
     push: (string) => void,
     loadChatRecords: (string) => void,
-    wechatId: string
+    wechatId: string,
+    chatRecords: Array<*>
 };
 
 
 const styles = theme => ({
     root: {
         width: '100%',
-        maxWidth: 360,
         backgroundColor: theme.palette.background.paper,
-        marginBottom: 30
+        marginBottom: 30,
+        display: 'flex',
+        justifyContent: 'center'
     },
+    progress: {
+        marginTop: 80,
+        marginBottom: 80,
+        color: theme.palette.primary.light
+    }
 });
 
 class LogPage extends React.Component<Props> {
@@ -38,60 +46,29 @@ class LogPage extends React.Component<Props> {
         loadChatRecords(wechatId);
     }
 
+    renderChatRecords(chatRecords) {
+
+        return (<List>
+            {chatRecords.map((record) => {
+
+                const primaryText = record.replyContent;
+                const secondaryText = record.gmtSent + '      ' +
+                    record.toRemarkName + '(' + record.toNickName + '):' + record.chatContent;
+                return (<ListItem>
+                    <ListItemText primary={primaryText} secondary={secondaryText}/>
+                </ListItem>)
+            })}
+        </List>)
+    }
+
     render() {
-        const {classes, ...frameContainerProps} = this.props;
+        const {classes, chatRecords, ...frameContainerProps} = this.props;
 
         return (
             <FrameContainer {...frameContainerProps}>
                 <div className={classes.root}>
-                    <List>
-                        <ListItem>
-                            <ListItemText primary="回复内容：你也好呀！" secondary="2018-05-17 12:13 12"/>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="回复内容：我是计算机科学技术学院的学生" secondary="2018-05-17 12:10 09"/>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="回复内容：软件工程专业" secondary="2018-05-17 12:08 11"/>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="回复内容：现在大四了" secondary="2018-05-17 12:07 19"/>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="回复内容：学校名字叫北华大学" secondary="2018-05-17 12:06 01"/>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="回复内容：没事，不用谢" secondary="2018-05-17 12:05 40"/>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="回复内容：再见，下次再聊" secondary="2018-05-17 12:04 50"/>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="回复内容：再见，下次再聊" secondary="2018-05-17 12:04 50"/>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="回复内容：再见，下次再聊" secondary="2018-05-17 12:04 50"/>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="回复内容：再见，下次再聊" secondary="2018-05-17 12:04 50"/>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="回复内容：再见，下次再聊" secondary="2018-05-17 12:04 50"/>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="回复内容：再见，下次再聊" secondary="2018-05-17 12:04 50"/>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="回复内容：再见，下次再聊" secondary="2018-05-17 12:04 50"/>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="回复内容：再见，下次再聊" secondary="2018-05-17 12:04 50"/>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="回复内容：再见，下次再聊" secondary="2018-05-17 12:04 50"/>
-                        </ListItem>
-
-                    </List>
+                    {chatRecords ? this.renderChatRecords(chatRecords) :
+                        <CircularProgress className={classes.progress} thickness={7}/>}
                 </div>
             </FrameContainer>);
     }
@@ -101,7 +78,8 @@ const mapStateToProps = (state) => {
     return {
         menuStatus: state.app.menuStatus,
         bottomNavCheckedIndex: state.app.bottomNavCheckedIndex,
-        wechatId: getWechatId(state)
+        wechatId: getWechatId(state),
+        chatRecords: state.app.chatRecords
     }
 };
 
