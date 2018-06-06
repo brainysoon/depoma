cb9420396fa999c2b04dce2819612b11afebcc59
brainysoon <brainysoon@gmail.com>
2018-06-06 08:44:37 +0800

    [ Back-6 ] - Fix robot img config.

--- eve/src/share/constant/configConstants.js
+++ eve/src/share/constant/configConstants.js
@@ -1,4 +1,5 @@
 export const SERVER_API_BASE_URL_PRODUCTION = 'http://123.207.136.243:8080/api/v1';
+export const SERVER_RESOURCES_BASE_URL_PRODUCTION = 'http://123.207.136.243';
 export const SERVER_API_BASE_URL_DEV = 'http://localhost:8080/api/v1';
 
 export const SETTING_TOGGLE_PRODUCTION = 'production';
